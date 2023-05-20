6af590fe57eeba842ffda8795a2dc7b7b6985242
VictorGarrett <victor_garrett@hotmail.com>
2023-05-20 18:04:29 -0300

    rule generation done

--- generateRules.m
+++ generateRules.m
@@ -1,5 +1,38 @@
-function fis = generateRules(fis, inputData, outputData)
+function fisout = generateRules(fis, inputData, outputData)
 
+    fisout = fis;
+    %initialize with default rules (weight = 0)
+    for i = 1:5
+        for j = 1:5
+            for k = 1:5
+                rulecube(i, j, k) = fisrule([i j k 1 0 1], 3);
+            end
+        end
+    end
+    
+    %   Generate rules from data, if a generated rule is in conflict with a
+    %previous rule, overwrite if its weight is larger.
+    %   Every pair [activations ruleinputs] has which mf has been most
+    %activated, together with its activation value.
+    for i = 1:length(inputData)
+        for j = 1:3 %for every input variable
+            %fuzzify inputs, get which mf returns the largest value
+            [activations(j), ruleinputs(j)] = max(evalmf(fis.Inputs(j).MembershipFunctions, inputData(i, j)));
+        end
+        %Generate the rule. The rule is created from the mfs which the
+        %input value most activates and the given output. The weight of the
+        %rule is the product of every mf activation.
+        newrule = fisrule([ruleinputs outputData(i) prod(activations) 1], 3);
 
+        %overwrite only if weight is larger
+        if rulecube(ruleinputs(1), ruleinputs(2), ruleinputs(3)).Weight < newrule.Weight
+           rulecube(ruleinputs(1), ruleinputs(2), ruleinputs(3)) = newrule;
+        end
 
+    end
+
+    %write all rules to the output system
+    for i = 1:numel(rulecube)
+        fisout = addRule(fisout, [rulecube(i).Antecedent rulecube(i).Consequent rulecube(i).Weight 1]);
+    end
 end
--- prepareData.m
+++ prepareData.m
@@ -1,4 +1,16 @@
+
+%prepares data from a file to generate rules
+
+%every row of the inputs and outputs matrix is a diferent training example
+
+%the inputs matrix has 3 columns, one for each variable (qPa, pulso, resp)
+
+%the outputs matrix has 1 column, it contains the index of the associated
+%category (1 = critico, 2 = instavel, 3 = potencialmente estavel, 4 = estavel)
 function [inputs, outputs] = prepareData(file)
-    inputs = zeros(800, 3);
-    outputs = zeros(800, 1);
+
+    %TODO
+
+    inputs = zeros(800, 3);%TEST ONLY
+    outputs = ones(800, 1);%TEST ONLY
 end
